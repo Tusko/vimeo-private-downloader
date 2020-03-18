69bc41df8bf072e34cc46504c75dcf8b6f65957e
Tusko <tusko@photoinside.me>
2020-03-18 15:52:01 +0200

    added script to merge audio/video with ffmpeg

--- runnner.sh
+++ runnner.sh
@@ -1,8 +1,2 @@
-ffmpeg -i '1. Introduction.m4v' -i '1. Introduction.m4a' -c:v copy -c:a copy './converted/1. Introduction.mp4';
-ffmpeg -i '10.1. Customer service - Введение.m4v' -i '10.1. Customer service - Введение.m4a' -c:v copy -c:a copy './converted/10.1. Customer service - Введение.mp4';
-ffmpeg -i '10.2. Правила и стандарты сервиса.m4v' -i '10.2. Правила и стандарты сервиса.m4a' -c:v copy -c:a copy './converted/10.2. Правила и стандарты сервиса.mp4';
-ffmpeg -i '10.3. Таблица ответственности.m4v' -i '10.3. Таблица ответственности.m4a' -c:v copy -c:a copy './converted/10.3. Таблица ответственности.mp4';
-ffmpeg -i '10.4. Работа с клиентами в социальных сетях.m4v' -i '10.4. Работа с клиентами в социальных сетях.m4a' -c:v copy -c:a copy './converted/10.4. Работа с клиентами в социальных сетях.mp4';
-ffmpeg -i '10.5. Заключение.m4v' -i '10.5. Заключение.m4a' -c:v copy -c:a copy './converted/10.5. Заключение.mp4';
-ffmpeg -i '11.1. Зачем это вам?.m4v' -i '11.1. Зачем это вам?.m4a' -c:v copy -c:a copy './converted/11.1. Зачем это вам?.mp4';
-ffmpeg -i '11.2. Что такое деньги?.m4v' -i '11.2. Что такое деньги?.m4a' -c:v copy -c:a copy './converted/11.2. Что такое деньги?.mp4';
+ffmpeg -i 'Introduction.m4v' -i 'Introduction.m4a' -c:v copy -c:a copy './converted/Introduction.mp4';
+ffmpeg -i 'Customer service.m4v' -i 'Customer service.m4a' -c:v copy -c:a copy './converted/Customer service.mp4';
