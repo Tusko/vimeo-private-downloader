a046dc65f695620ddd8d8ceebaf3cd05ecf4adde
ftitreefly <ftitree_fly@hotmail.com>
2020-03-21 01:23:13 +0800

    add sub folder's name path 'parts' and 'converted'

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # A candy bash for combine videos
-# cody by ftitreefly
+# code by ftitreefly
 # 2020-03-19
-ls *.m4a *.m4v | sed 's/.m4a//g;s/.m4v//g' | uniq | \
-awk '{print "echo Runing Task "NR" - " $0}{m4a=$0".m4a";m4v=$0".m4v";mp4=$0".mp4"; printf("ffmpeg -y -loglevel quiet -i \"%s\" -i \"%s\" -c copy \"%s\"\n", m4a, m4v, mp4) }' \
+ls parts/*.m4a parts/*.m4v | sed 's/.m4a//g;s/.m4v//g;s/parts\///g' | uniq | \
+awk '{print "echo Runing Task "NR" - " $0}{m4a="parts/"$0".m4a";m4v="parts/"$0".m4v";mp4="converted/"$0".mp4"; printf("ffmpeg -y -loglevel quiet -i \"%s\" -i \"%s\" -c copy \"%s\"\n", m4a, m4v, mp4) }' \
 > combine.sh
 
 chmod +x combine.sh
